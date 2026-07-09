187ed0cea517400cfdf596aa575b3c58fee01e6d
dikshya-in-github <ghisingdeeku@gmail.com>
2026-07-09 07:53:54 +0545

    July 9

--- src/main/java/io/virinchi/App.java
+++ src/main/java/io/virinchi/App.java
@@ -1,5 +1,10 @@
 package io.virinchi;
 
+import io.virinchi.model.UserClass;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App {
     /*
         JPA - Java Persistence API
@@ -11,6 +16,29 @@ public class App {
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /*
+        Session create garera database ma hibernate ko through table ni banaunu paryo,
+        data ni pathauna paryo.
+
+        Session banauxa session factory lay
+         */
+
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        //SessionFactory bata configuration settings taneko
+
+        Session session = sf.openSession();
+        //Ek session ma multiple hibernate transaction huna sakxa, tyo open gareko
+
+        UserClass uc = new UserClass();
+        uc.setName("Jack");
+        //Model ko class jun aba table hunxa tesko column ko data rakheko
+
+        session.persist(uc);
+        //session.persist baneko, yo session ma hamro model ko class ko table banna paryo or
+        //banako table ma data basna pryo
+        //persist le data rakhne help garxa(save vaneko jastai ho)
+        session.beginTransaction().commit();
+        //commit le kam suru garxa.
+        session.close();
     }
 }
--- src/main/java/io/virinchi/model/UserClass.java
+++ src/main/java/io/virinchi/model/UserClass.java
@@ -14,5 +14,22 @@ public class UserClass {
     private int id;
     private String name;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
+//Exam ma hibernate ko auta ques auxa which is
+//
